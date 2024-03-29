1f8df69844176e763c6fbf248679a71279730a7b
Clark <livioco.clarkcyrus@ue.edu.ph>
2024-03-29 13:32:27 +0800

    Test Clear Migrations and dbqlite

--- salad_navotenos/settings.py
+++ salad_navotenos/settings.py
@@ -43,11 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'users',
     'main',
-    'customer',
-    'salad'
-    'users'
+    'salad',
     
 ]
 
